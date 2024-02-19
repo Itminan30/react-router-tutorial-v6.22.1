6bee1f6447a43f723a851bcc0f1b53d5be1e58ed
Itminan30 <itminan30@gmail.com>
2024-02-20 03:14:11 +0600

    Third Commit: The Root Route done

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './routes/Root'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>
+    element: <Root />
   }
 ])
 
