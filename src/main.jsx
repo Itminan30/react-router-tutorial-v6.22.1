9bd156df04dc08a877027ccad6c906d6526eac30
Itminan30 <itminan30@gmail.com>
2024-02-21 05:51:02 +0600

    Update Data done

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import ErrorPage from './ErrorPage'
 import Contact from './routes/Contact'
 import { contactLoader, contactsLoader as rootLoader } from './loaders/contactLoader'
 import { action as rootAction } from './actions/contactAction'
+import EditContact from './routes/Edit'
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
         path: "/contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: "/contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader
       }
     ]
   }
